1f92b6ef395209b1c15bc35e309bab659ed8778a
Sevvandi Kandanaarachchi <sevvandik@gmail.com>
2020-11-23 09:50:22 +0000

    version 0.2.0

--- inst/doc/airt.R
+++ inst/doc/airt.R
@@ -20,21 +20,70 @@ if (!requireNamespace("tidyr", quietly = TRUE)) {
       call. = FALSE)
 }
 
+
 ## ----load, message=FALSE-------------------------------------------------
 library(airt)
 library(ggplot2)
 library(tidyr)
 
-## ----example-------------------------------------------------------------
-data("classification")
-modout <- irtmodel(classification, vpara=FALSE)
+## ----example2------------------------------------------------------------
+data("classification_cts")
+df2 <- classification_cts
+max_item  <- max(df2)
+min_item <- 0
+max.item <- rep(max_item, dim(df2)[2])
+min.item <- rep(min_item, dim(df2)[2])
+df2 <- as.data.frame(df2)
+modout <- cirtmodel(df2, max.item, min.item)
+paras <- modout$model$param
 
-gdf <- prepare_for_plots(modout$model)
+gdf <- prepare_for_plots_crm(modout$model) 
+ggplot(gdf, aes(theta, z)) + geom_raster(aes(fill=pdf))  + xlab("theta") + facet_wrap(~Algorithm, nrow=2) + coord_fixed(ratio=1) + theme_bw()  +  scale_fill_viridis_c(option = "plasma")  
+
+## ----latenttrait---------------------------------------------------------
+
+obj <- latent_trait_analysis(df2,modout$model$param,min.item,max.item )
+
+dfl <- obj$longdf
+
+g1 <- ggplot(dfl, aes(Latent_Trait, value)) +  geom_point(aes(color=Algorithm)) + xlab("Latent Trait (Dataset Easiness)") + ylab("Performance")  + theme_bw() 
+g1
+
+## ----latent2-------------------------------------------------------------
+
+g3 <- ggplot(dfl, aes(Latent_Trait, value)) +  geom_point(aes(color=Algorithm)) + xlab("Latent Trait (Dataset Easiness)") + facet_wrap(~Algorithm, nrow=2) + coord_fixed(ratio=6) + ylab("Performance") + theme_bw() 
+g3
+
+
+## ----latent3-------------------------------------------------------------
+
+### Curve fitting - smoothing splines - latent trait
+g2 <- ggplot(dfl, aes(Latent_Trait, value)) +  geom_smooth(aes(color=Algorithm), se = FALSE, method = "gam", formula = y ~s(x, bs="cs"))+  xlab("Latent Trait (Dataset Easiness)") + ylab("Performance")  + theme_bw()  +theme(legend.position="bottom", legend.box = "horizontal")
+g2
+
+## ----latent 4------------------------------------------------------------
+latent <- obj$latent
+latent$proportions
+
+setColors <- setNames( latent$proportions$colour, latent$proportions$algorithm)
+
+df2 <- latent$latent
+df3 <- cbind(df2, y=1)
+df3 <- df3[ ,c(1,3,2)]
+g4 <- ggplot(df3, aes(x,y)) + geom_point(aes(color=Algorithm),size=2, shape=15) + ylab("") + coord_fixed(ratio = 2) + theme(axis.title.y=element_blank(), axis.text.y=element_blank(),axis.ticks.y=element_blank()) + scale_color_manual(values = setColors) + xlab("Latent Trait") + theme(legend.position="bottom", legend.box="vertical", legend.margin=margin())  +guides(group=guide_legend(nrow=3))
+g4
+
+
+## ----example-------------------------------------------------------------
+data("classification_poly")
+modout <- pirtmodel(classification_poly, vpara=FALSE)
+
+gdf <- prepare_for_plots_poly(modout$model)
 ggplot(gdf, aes(Theta, value)) + geom_line(aes(color=Level)) + facet_wrap(.~Algorithm) + ylab("Probability") + ggtitle("Classification Algorithm Trace Lines") + theme_bw()
 
 ## ---- goodness, echo=TRUE------------------------------------------------
 # Model Goodness and Algorithm effectiveness
-good <- model_goodness(modout$model)
+good <- model_goodness_poly(modout$model)
 
 good_curves <- as.data.frame(good$curves)
 print(good_curves)
@@ -45,7 +94,7 @@ ggplot(good_df, aes(x,value)) + geom_point() + geom_line(aes(color = Algorithm),
 good$goodnessAUC
 
 ## ---- effectiveness1, echo=TRUE------------------------------------------
-eff <- effectiveness(modout$model)
+eff <- effectiveness_poly(modout$model)
 
 eff_curves <- as.data.frame(eff$actcurves)
 eff_df1 <- eff_curves %>% pivot_longer(cols=2:dim(eff_curves)[2], names_to=c("Algorithm"))
